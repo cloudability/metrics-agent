af45bb18d36e65f1dad2aa0a612b834ea225f777
David Arnold <davidxarnold@soundofcorduroy.com>
2018-07-25 20:16:41 -0600

    update raw get endpoint to retry on errors, updated contributing doc (#5)
    * update raw get endpoint to retry on errors, updated contributing doc
    
    * increase gometalinter deadline as it can timeout on ircleci
    
    * address PR comments / reqested changes
    
    * implemented requested changes, removed uneeded sleep on retry, fixed typo, moved defer in raw endpoint to be effective.
    
    * bumped version

--- retrieval/raw/raw_endpoint.go
+++ retrieval/raw/raw_endpoint.go
@@ -4,9 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/cloudability/metrics-agent/util"
 )
@@ -16,14 +20,16 @@ type Client struct {
 	HTTPClient  *http.Client
 	insecure    bool
 	bearerToken string
+	retries     uint
 }
 
 //NewClient creates a new raw.Client
-func NewClient(HTTPClient http.Client, insecure bool, bearerToken string) Client {
+func NewClient(HTTPClient http.Client, insecure bool, bearerToken string, retries uint) Client {
 	return Client{
 		HTTPClient:  &HTTPClient,
 		insecure:    insecure,
 		bearerToken: bearerToken,
+		retries:     retries,
 	}
 }
 
@@ -43,22 +49,44 @@ func (c *Client) createRequest(method, url string, body io.Reader) (*http.Reques
 }
 
 //GetRawEndPoint retrives the body of HTTP response from a given sourcename, working directory, and URL
-func (c *Client) GetRawEndPoint(sourceName string, workDir *os.File, URL string) (rawRespFile *os.File, rerr error) {
+func (c *Client) GetRawEndPoint(sourceName string,
+	workDir *os.File, URL string) (rawRespFile *os.File, err error) {
 
-	var empty os.File
+	attempts := c.retries + 1
+
+	for i := uint(0); i < attempts; i++ {
+		if i > 0 {
+			time.Sleep(time.Duration(int64(math.Pow(2, float64(i)))) * time.Second)
+		}
+		rawRespFile, err = downloadToFile(c, sourceName, workDir, URL, i)
+		if err == nil {
+			return rawRespFile, nil
+		}
+		log.Printf("%v URL: %s retrying: %v", err, URL, i+1)
+	}
+	return nil, err
+}
+
+func downloadToFile(c *Client, sourceName string,
+	workDir *os.File, URL string, retryCount uint) (rawRespFile *os.File, rerr error) {
+
 	var fileExt string
 
 	req, err := c.createRequest("GET", URL, nil)
 	if err != nil {
-		return &empty, errors.New("Unable to create raw request for " + sourceName)
+		return rawRespFile, errors.New("Unable to create raw request for " + sourceName)
 	}
 
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
-		return &empty, err
+		return rawRespFile, errors.New("Unable to connect")
 	}
 
 	defer util.SafeClose(resp.Body.Close, &rerr)
+
+	if !(resp.StatusCode >= 200 && resp.StatusCode <= 299) {
+		return rawRespFile, fmt.Errorf("Invalid response %s", strconv.Itoa(resp.StatusCode))
+	}
 
 	ct := resp.Header.Get("Content-Type")
 
@@ -72,13 +100,12 @@ func (c *Client) GetRawEndPoint(sourceName string, workDir *os.File, URL string)
 
 	rawRespFile, err = os.Create(workDir.Name() + "/" + sourceName + fileExt)
 	if err != nil {
-		return &empty, errors.New("Unable to create raw metric file")
+		return rawRespFile, errors.New("Unable to create raw metric file")
 	}
 
 	_, err = io.Copy(rawRespFile, resp.Body)
 	if err != nil {
-		fmt.Print(err)
-		return &empty, errors.New("Error writing file: " + rawRespFile.Name())
+		return rawRespFile, errors.New("Error writing file: " + rawRespFile.Name())
 	}
 
 	return rawRespFile, err
--- retrieval/raw/raw_endpoint_test.go
+++ retrieval/raw/raw_endpoint_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func TestGetRawEndPoint(t *testing.T) {
+	t.Parallel()
 
 	t.Run("ensure that a file is created from a raw endpoint", func(t *testing.T) {
-		t.Parallel()
 
 		httpClient := http.DefaultClient
 
@@ -21,6 +21,7 @@ func TestGetRawEndPoint(t *testing.T) {
 			*httpClient,
 			true,
 			"",
+			2,
 		)
 
 		wd, _ := ioutil.TempDir("", "raw_endpoint_test")
@@ -45,6 +46,53 @@ func TestGetRawEndPoint(t *testing.T) {
 
 		if sF.Size() != tF.Size() {
 			t.Error("Source file does not match source")
+		}
+
+	})
+
+	t.Run("ensure error when non http 200-299 returned", func(t *testing.T) {
+
+		httpClient := http.DefaultClient
+
+		client := NewClient(
+			*httpClient,
+			true,
+			"",
+			2,
+		)
+
+		wd, _ := ioutil.TempDir("", "raw_endpoint_test")
+		workingDir, _ := os.Open(wd)
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(404)
+		}))
+		defer ts.Close()
+
+		_, err := client.GetRawEndPoint("heapster", workingDir, ts.URL)
+		if err == nil {
+			t.Error("Server returned invalid response code but function did not raise error")
+		}
+
+	})
+
+	t.Run("ensure error when unable to connect", func(t *testing.T) {
+
+		httpClient := http.DefaultClient
+
+		client := NewClient(
+			*httpClient,
+			true,
+			"",
+			2,
+		)
+
+		wd, _ := ioutil.TempDir("", "raw_endpoint_test")
+		workingDir, _ := os.Open(wd)
+
+		_, err := client.GetRawEndPoint("heapster", workingDir, "http://localhost:1234")
+		if err == nil {
+			t.Error("Unable to to connect to server but function did not raise error")
 		}
 
 	})
