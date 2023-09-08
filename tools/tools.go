4c2effd220fed88603f6751a6beb6dccffe31c6a
jdhudson3 <43144225+jdhudson3@users.noreply.github.com>
2023-09-08 12:14:22 -0400

    dependency updates

--- tools/tools.go
+++ tools/tools.go
@@ -4,10 +4,6 @@
 package tools
 
 import (
-	// linter pinned here so `go mod tidy` can be run without removing this dependency
-	// used for `make lint` cmd
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-
 	// e2e test deps pinned here so `go mod tidy` can be run without removing these dependencies
 	// used in `make test-e2e-X.XX` and `make test-e2e-all` cmds
 	_ "github.com/google/cadvisor/info/v1"
