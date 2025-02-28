e14179198782799f8cb0d1138fefb523054ce682
Daniel Spray <dspray@apptio.com>
2025-02-27 16:39:32 -0800

    local linter is not working

--- retrieval/raw/models.go
+++ retrieval/raw/models.go
@@ -139,6 +139,7 @@ type CldyPod struct {
 }
 
 // CldyObjectMeta represents k8s ObjectMeta but strips unnecessary metadata
+//
 //nolint:lll
 type CldyObjectMeta struct {
 	Name                       string                   `json:"name,omitempty" protobuf:"bytes,1,opt,name=name"`
@@ -158,6 +159,7 @@ type CldyObjectMeta struct {
 }
 
 // CldyManagedFieldsEntry represents k8s ManagedFieldsEntry but strips unnecessary metadata
+//
 //nolint:lll
 type CldyManagedFieldsEntry struct {
 	Manager    string                            `json:"manager,omitempty" protobuf:"bytes,1,opt,name=manager"`
