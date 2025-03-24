54ed99ba7944b691161d0d5c91f43cad5e07767d
Daniel Spray <86271298+daniel-spray@users.noreply.github.com>
2025-03-24 12:10:42 -0700

    Reduce Metrics Payload Size [2] (#298)

--- retrieval/k8s/k8s_stats.go
+++ retrieval/k8s/k8s_stats.go
@@ -21,7 +21,7 @@ const (
 )
 
 func StartUpInformers(clientset kubernetes.Interface, clusterVersion float64,
-	resyncInterval int, stopCh chan struct{}) (map[string]*cache.SharedIndexInformer, error) {
+	resyncInterval int, parseMetricsData bool, stopCh chan struct{}) (map[string]*cache.SharedIndexInformer, error) {
 	factory := informers.NewSharedInformerFactory(clientset, time.Duration(resyncInterval)*time.Hour)
 
 	// v1Sources
@@ -44,11 +44,6 @@ func StartUpInformers(clientset kubernetes.Interface, clusterVersion float64,
 		cronJobsInformer = factory.Batch().V1().CronJobs().Informer()
 	}
 
-	// runs in background, starts all informers that are a part of the factory
-	factory.Start(stopCh)
-	// wait until all informers have successfully synced
-	factory.WaitForCacheSync(stopCh)
-
 	var clusterInformers = map[string]*cache.SharedIndexInformer{
 		"replicationcontrollers": &replicationControllerInformer,
 		"services":               &servicesInformer,
@@ -63,19 +58,33 @@ func StartUpInformers(clientset kubernetes.Interface, clusterVersion float64,
 		"jobs":                   &jobsInformer,
 		"cronjobs":               &cronJobsInformer,
 	}
+
+	for _, informer := range clusterInformers {
+		transform := GetTransformFunc(parseMetricsData)
+		err := (*informer).SetTransform(transform)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// runs in background, starts all informers that are a part of the factory
+	factory.Start(stopCh)
+	// wait until all informers have successfully synced
+	factory.WaitForCacheSync(stopCh)
+
 	return clusterInformers, nil
 }
 
 // GetK8sMetricsFromInformer loops through all k8s resource informers in kubeAgentConfig writing each to the WSD
 func GetK8sMetricsFromInformer(informers map[string]*cache.SharedIndexInformer,
-	workDir *os.File, parseMetricData bool) error {
+	workDir *os.File) error {
 	for resourceName, informer := range informers {
 		// Cronjob informer will be nil if k8s version is less than 1.21, if so skip getting the list of cronjobs
 		if *informer == nil {
 			continue
 		}
 		resourceList := (*informer).GetIndexer().List()
-		err := writeK8sResourceFile(workDir, resourceName, resourceList, parseMetricData)
+		err := writeK8sResourceFile(workDir, resourceName, resourceList)
 
 		if err != nil {
 			return err
@@ -86,7 +95,7 @@ func GetK8sMetricsFromInformer(informers map[string]*cache.SharedIndexInformer,
 
 // writeK8sResourceFile creates a new file in the upload sample directory for the resourceName passed in and writes data
 func writeK8sResourceFile(workDir *os.File, resourceName string,
-	resourceList []interface{}, parseMetricData bool) (rerr error) {
+	resourceList []interface{}) (rerr error) {
 
 	file, err := os.OpenFile(workDir.Name()+"/"+resourceName+".jsonl",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -96,13 +105,11 @@ func writeK8sResourceFile(workDir *os.File, resourceName string,
 	datawriter := bufio.NewWriter(file)
 
 	for _, k8Resource := range resourceList {
-
-		if parseMetricData {
-			k8Resource = sanitizeData(k8Resource)
+		if shouldSkipResource(k8Resource) {
+			continue
 		}
 
 		data, err := json.Marshal(k8Resource)
-
 		if err != nil {
 			return errors.New("error: unable to marshal resource: " + resourceName)
 		}
@@ -124,40 +131,84 @@ func writeK8sResourceFile(workDir *os.File, resourceName string,
 	return err
 }
 
-// nolint: gocyclo
+func shouldSkipResource(k8Resource interface{}) bool {
+	// safe buffer to allow for longer lived resources to be ingested correctly
+	previousHour := time.Now().UTC().Add(-1 * time.Hour)
+	switch resource := k8Resource.(type) {
+	case *v1batch.Job:
+		return shouldSkipJob(previousHour, resource)
+	case *corev1.Pod:
+		return shouldSkipPod(previousHour, resource)
+	case *v1apps.ReplicaSet:
+		return resource.Status.Replicas == 0 && previousHour.After(resource.CreationTimestamp.Time)
+	}
+	return false
+}
+
+func shouldSkipJob(previousHour time.Time, resource *v1batch.Job) bool {
+	if resource.Status.CompletionTime != nil &&
+		previousHour.After(resource.Status.CompletionTime.Time) {
+		return true
+	}
+	if resource.Status.Failed > 0 {
+		for _, condition := range resource.Status.Conditions {
+			if condition.Type == v1batch.JobFailed {
+				if previousHour.After(condition.LastTransitionTime.Time) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func shouldSkipPod(previousHour time.Time, resource *corev1.Pod) bool {
+	if resource.Status.Phase == corev1.PodSucceeded || resource.Status.Phase == corev1.PodFailed {
+		canSkip := true
+		for _, v := range resource.Status.ContainerStatuses {
+			if v.State.Terminated != nil && v.State.Terminated.FinishedAt.After(previousHour) {
+				canSkip = false
+			}
+		}
+		return canSkip
+	}
+	return false
+}
+
+// sanitizeData removes information from kubernetes resources for customer security purposes
+// nolint:gocyclo
 func sanitizeData(to interface{}) interface{} {
 	switch to.(type) {
 	case *corev1.Pod:
 		return sanitizePod(to)
 	case *v1apps.DaemonSet:
 		cast := to.(*v1apps.DaemonSet)
-		sanitizeMeta(&cast.ObjectMeta)
 		cast.Spec.Template = corev1.PodTemplateSpec{}
 		cast.Spec.RevisionHistoryLimit = nil
 		cast.Spec.UpdateStrategy = v1apps.DaemonSetUpdateStrategy{}
 		cast.Spec.MinReadySeconds = 0
 		cast.Spec.RevisionHistoryLimit = nil
+		sanitizeMeta(&cast.ObjectMeta)
 		return cast
 	case *v1apps.ReplicaSet:
 		cast := to.(*v1apps.ReplicaSet)
-		sanitizeMeta(&cast.ObjectMeta)
 		cast.Spec.Replicas = nil
 		cast.Spec.Template = corev1.PodTemplateSpec{}
 		cast.Spec.MinReadySeconds = 0
+		sanitizeMeta(&cast.ObjectMeta)
 		return cast
 	case *v1apps.Deployment:
 		cast := to.(*v1apps.Deployment)
-		sanitizeMeta(&cast.ObjectMeta)
 		cast.Spec.Template = corev1.PodTemplateSpec{}
 		cast.Spec.Replicas = nil
 		cast.Spec.Strategy = v1apps.DeploymentStrategy{}
 		cast.Spec.MinReadySeconds = 0
 		cast.Spec.RevisionHistoryLimit = nil
 		cast.Spec.ProgressDeadlineSeconds = nil
+		sanitizeMeta(&cast.ObjectMeta)
 		return cast
 	case *v1batch.Job:
 		cast := to.(*v1batch.Job)
-		sanitizeMeta(&cast.ObjectMeta)
 		cast.Spec.Template = corev1.PodTemplateSpec{}
 		cast.Spec.Parallelism = nil
 		cast.Spec.Completions = nil
@@ -167,16 +218,16 @@ func sanitizeData(to interface{}) interface{} {
 		cast.Spec.TTLSecondsAfterFinished = nil
 		cast.Spec.CompletionMode = nil
 		cast.Spec.Suspend = nil
+		sanitizeMeta(&cast.ObjectMeta)
 		return cast
 	case *v1batch.CronJob:
 		cast := to.(*v1batch.CronJob)
-		sanitizeMeta(&cast.ObjectMeta)
 		// cronjobs have no Selector
 		cast.Spec = v1batch.CronJobSpec{}
+		sanitizeMeta(&cast.ObjectMeta)
 		return cast
 	case *corev1.Service:
 		cast := to.(*corev1.Service)
-		sanitizeMeta(&cast.ObjectMeta)
 		cast.Spec.Ports = nil
 		cast.Spec.ClusterIP = ""
 		cast.Spec.ClusterIPs = nil
@@ -194,16 +245,15 @@ func sanitizeData(to interface{}) interface{} {
 		cast.Spec.AllocateLoadBalancerNodePorts = nil
 		cast.Spec.LoadBalancerClass = nil
 		cast.Spec.InternalTrafficPolicy = nil
+		sanitizeMeta(&cast.ObjectMeta)
 		return cast
 	case *corev1.ReplicationController:
 		cast := to.(*corev1.ReplicationController)
-		sanitizeMeta(&cast.ObjectMeta)
 		cast.Spec.Replicas = nil
 		cast.Spec.Template = nil
 		cast.Spec.MinReadySeconds = 0
+		sanitizeMeta(&cast.ObjectMeta)
 		return cast
-	case *corev1.Namespace:
-		return sanitizeNamespace(to)
 	case *corev1.PersistentVolume:
 		cast := to.(*corev1.PersistentVolume)
 		sanitizeMeta(&cast.ObjectMeta)
@@ -220,19 +270,69 @@ func sanitizeData(to interface{}) interface{} {
 	return to
 }
 
+// trimData removes unneeded kubernetes resource fields
+// nolint:gocyclo
+func trimData(to interface{}) interface{} {
+	switch to.(type) {
+	case *corev1.Pod:
+		return trimPod(to)
+	case *v1apps.DaemonSet:
+		cast := to.(*v1apps.DaemonSet)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *v1apps.ReplicaSet:
+		cast := to.(*v1apps.ReplicaSet)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *v1apps.Deployment:
+		cast := to.(*v1apps.Deployment)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *v1batch.Job:
+		cast := to.(*v1batch.Job)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *v1batch.CronJob:
+		cast := to.(*v1batch.CronJob)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *corev1.Service:
+		cast := to.(*corev1.Service)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *corev1.ReplicationController:
+		cast := to.(*corev1.ReplicationController)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *corev1.Namespace:
+		return trimNamespace(to)
+	case *corev1.PersistentVolume:
+		cast := to.(*corev1.PersistentVolume)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *corev1.PersistentVolumeClaim:
+		cast := to.(*corev1.PersistentVolumeClaim)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	case *corev1.Node:
+		cast := to.(*corev1.Node)
+		trimMeta(&cast.ObjectMeta)
+		return cast
+	}
+	return to
+}
+
 func sanitizeMeta(objectMeta *metav1.ObjectMeta) {
+	objectMeta.Finalizers = nil
+}
+
+func trimMeta(objectMeta *metav1.ObjectMeta) {
 	objectMeta.ManagedFields = nil
 	delete(objectMeta.Annotations, KubernetesLastAppliedConfig)
-	objectMeta.Finalizers = nil
 }
 
 func sanitizePod(to interface{}) interface{} {
 	cast := to.(*corev1.Pod)
-
-	// stripping env var and related data from the object
-	(*cast).ObjectMeta.ManagedFields = nil
-	delete((*cast).ObjectMeta.Annotations, KubernetesLastAppliedConfig)
-
 	for j, container := range (*cast).Spec.Containers {
 		(*cast).Spec.Containers[j] = sanitizeContainer(container)
 	}
@@ -242,8 +342,22 @@ func sanitizePod(to interface{}) interface{} {
 	return cast
 }
 
+func trimPod(to interface{}) interface{} {
+	cast := to.(*corev1.Pod)
+	// removing env var and related data from the object
+	(*cast).ObjectMeta.ManagedFields = nil
+	delete((*cast).ObjectMeta.Annotations, KubernetesLastAppliedConfig)
+
+	for j, container := range (*cast).Spec.Containers {
+		(*cast).Spec.Containers[j] = trimContainer(container)
+	}
+	for j, container := range (*cast).Spec.InitContainers {
+		(*cast).Spec.InitContainers[j] = trimContainer(container)
+	}
+	return cast
+}
+
 func sanitizeContainer(container corev1.Container) corev1.Container {
-	container.Env = nil
 	container.Command = nil
 	container.Args = nil
 	container.ImagePullPolicy = ""
@@ -256,8 +370,23 @@ func sanitizeContainer(container corev1.Container) corev1.Container {
 	return container
 }
 
-func sanitizeNamespace(to interface{}) interface{} {
+func trimContainer(container corev1.Container) corev1.Container {
+	container.Env = nil
+	return container
+}
+
+func trimNamespace(to interface{}) interface{} {
 	cast := to.(*corev1.Namespace)
 	(*cast).ObjectMeta.ManagedFields = nil
 	return cast
+}
+
+func GetTransformFunc(parseMetricsData bool) func(resource interface{}) (interface{}, error) {
+	return func(resource interface{}) (interface{}, error) {
+		if parseMetricsData {
+			resource = sanitizeData(resource)
+		}
+		resource = trimData(resource)
+		return resource, nil
+	}
 }
